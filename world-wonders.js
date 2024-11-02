88faa507d0ba4cd420336e4e4f3dba13f6d83878
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-02 13:10:27 +0200

    added images to the html

--- world-wonders.js
+++ world-wonders.js
@@ -1,24 +1,35 @@
-console.log("World Wonders");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("World Wonders");
 
-const instance = axios.create({
-    baseURL: "https://www.world-wonders-api.org/v0"
-});
-
-const fetchWorldWonders = async () => {
-    const response = await instance.get("/wonders");
-    response.data.forEach(wonder => {
-        Object.keys(wonder).forEach(element => {
-            if (element === "name")
-                console.log("Name: " + wonder[element]);
-            if (element === "summary")
-                console.log("Summary: " + wonder[element]);
-            if (element === "links"){
-                const links = wonder[element];
-                console.log("Link: " + Object.values(links)[0]);
-                console.log("Image: " + Object.values(links)[4][1]);
-            }
-        });
+    const instance = axios.create({
+        baseURL: "https://www.world-wonders-api.org/v0"
     });
-};
 
-fetchWorldWonders();
+    const fetchWorldWonders = async () => {
+        const response = await instance.get("/wonders");
+        response.data.forEach(wonder => {
+            Object.keys(wonder).forEach(element => {
+                if (element === "name")
+                    console.log("Name: " + wonder[element]);
+                if (element === "summary")
+                    console.log("Summary: " + wonder[element]);
+                if (element === "links") {
+                    let links = wonder[element];
+                    console.log("Link: " + Object.values(links)[0]);
+
+                    let imageContainer = document.getElementById("imageContainer")
+                    let imagesrc = Object.values(links)[4][1]
+                    console.log("Image: " + imagesrc);
+
+                    let imgElement = document.createElement("img");
+                    imgElement.src = imagesrc;
+                    imgElement.alt = "World Wonder Image";
+
+                    imageContainer.appendChild(imgElement);
+                }
+            });
+        });
+    };
+
+    fetchWorldWonders();
+});
