e91ff1177b630c8a16f5c5b1f8652177bbb8161d
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-02 02:38:08 +0200

    fetched the url using axios

--- world-wonders.js
+++ world-wonders.js
@@ -1,1 +1,11 @@
 console.log("World Wonders");
+
+const instance = axios.create({
+    baseURL : "https://www.world-wonders-api.org/v0"
+});
+
+const fetchWorldWonders = async()=>{
+    const response = await instance.get("/wonders")
+    console.log(typeof response.data)
+};
+fetchWorldWonders();
