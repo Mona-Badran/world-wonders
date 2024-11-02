2157c0ba2523d59b8e28af2b6135047ac90d1580
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-02 09:45:51 +0200

    looped through world wondersusing foreach instead of for

--- world-wonders.js
+++ world-wonders.js
@@ -1,16 +1,19 @@
 console.log("World Wonders");
 
 const instance = axios.create({
-    baseURL : "https://www.world-wonders-api.org/v0"
+    baseURL: "https://www.world-wonders-api.org/v0"
 });
 
-const fetchWorldWonders = async()=>{
-    const response = await instance.get("/wonders")
-    let worldWonders = response.data;
-    let keys = Object.keys(response.data);
-    for(let i = 0; i < worldWonders.length; i++){
-        console.log(worldWonders[keys[i]])
-    }
-    console.log(worldWonders)
+const fetchWorldWonders = async () => {
+    const response = await instance.get("/wonders");
+    response.data.forEach(element => {
+        console.log(typeof element)
+    });
+
+    // let worldWonders = response.data[keysWorldWonders[i]];
+    // for (let j = 0; j < worldWonders.length; j++) {
+    //     console.log(worldWonders[Object.keys(worldWonders)[i]]);
+    // }
+
 };
 fetchWorldWonders();
