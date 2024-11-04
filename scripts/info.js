441261875c782f40067bdb372488d074183c3cdd
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-04 09:41:50 +0200

    corrected the looping to only include the elemnt chosen

--- scripts/info.js
+++ scripts/info.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     const fetchWorldWonders = async () => {
         const response = await instance.get("/wonders");
-
+        
         response.data.forEach(wonder => {
 
             let name = "";
@@ -27,48 +27,52 @@ document.addEventListener("DOMContentLoaded", () => {
             Object.keys(wonder).forEach(element => {
                 if (element === "name") {
                     name = wonder[element];
-                    console.log("Name: " + wonder[element]);
+                    // console.log("Name: " + wonder[element]);
                 }
-                if (element === "summary") {
-                    summary = wonder[element];
-                    console.log("summary: " + wonder[element]);
-                }
-                if (element === "location") {
-                    location = wonder[element];
-                    console.log("location: " + wonder[element]);
-                }
-                if (element === "build_year") {
-                    build_year = wonder[element];
-                    console.log("build_year: " + wonder[element]);
-                }
-                if (element === "time_period") {
-                    time_period = wonder[element];
-                    console.log("time_period: " + wonder[element]);
-                }
-                if (element === "links") {
-                    links_object = wonder[element];
-                    wiki_link = links_object.wiki;
-                    britannica_link = links_object.britannica;
-                    google_maps_link = links_object.google_maps;
-                    trip_advisor_link = links_object.trip_advisor;
+                if (name === param) {
+                    if (element === "name") {
+                        console.log("Name: " + wonder[element]);
+                    }
+                    if (element === "summary") {
+                        summary = wonder[element];
+                        // console.log("summary: " + wonder[element]);
+                    }
+                    if (element === "location") {
+                        location = wonder[element];
+                        // console.log("location: " + wonder[element]);
+                    }
+                    if (element === "build_year") {
+                        build_year = wonder[element];
+                        // console.log("build_year: " + wonder[element]);
+                    }
+                    if (element === "time_period") {
+                        time_period = wonder[element];
+                        // console.log("time_period: " + wonder[element]);
+                    }
+                    if (element === "links") {
+                        links_object = wonder[element];
+                        wiki_link = links_object.wiki;
+                        britannica_link = links_object.britannica;
+                        google_maps_link = links_object.google_maps;
+                        trip_advisor_link = links_object.trip_advisor;
 
-                    // console.log("trip_advisor_link: " + trip_advisor_link);
-                    // console.log("britannica_link: " + britannica_link);
-                    // console.log("google_maps_link: " + google_maps_link);
+                        // console.log("trip_advisor_link: " + trip_advisor_link);
+                        // console.log("britannica_link: " + britannica_link);
+                        // console.log("google_maps_link: " + google_maps_link);
 
-                    // console.log("images: " + typeof links_object.images);
+                        // console.log("images: " + typeof links_object.images);
 
-                    images_src_arr = Object.values(links_object.images);
-                    // console.log("images arr: " + typeof images_src_arr);
+                        images_src_arr = Object.values(links_object.images);
+                        // console.log("images arr: " + typeof images_src_arr);
 
-                    const imageContainer = document.getElementById("imageContainer");
-                    images_src_arr.forEach(src => {
-                        const img = document.createElement("img");
-                        img.src = src;
-                        img.style.width = "200px";
-                        imageContainer.appendChild(img);
-                    });
-
+                        const imageContainer = document.getElementById("imageContainer");
+                        images_src_arr.forEach(src => {
+                            const img = document.createElement("img");
+                            img.src = src;
+                            img.style.width = "200px";
+                            imageContainer.appendChild(img);
+                        });
+                    }
                 }
             });
             // let card = document.createElement("div")
